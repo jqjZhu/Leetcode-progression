8625ca444377fb688c0108bafab8507bb489a53d
Zqijing <your_email_here>
2020-07-11 19:55:50 -0700

    7/11

--- BFS/lc 314 Binary Tree Vertical Order Traversal.py
+++ BFS/lc 314 Binary Tree Vertical Order Traversal.py
@@ -26,6 +26,26 @@
 # 宽度优先遍历的空间复杂度为O(N)。
 # 总的空间复杂度为O(N)。
 
+# Using list as the default_factory, it is easy to group a sequence of key-value pairs into a dictionary of lists:
+
+# >>> s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+# >>> d = defaultdict(list)
+# >>> for k, v in s:
+# ...     d[k].append(v)
+# ...
+# >>> d.items()
+# [('blue', [2, 4]), ('red', [1]), ('yellow', [1, 3])]
+
+# Setting the default_factory to int makes the defaultdict useful for counting (like a bag or multiset in other languages):
+
+# >>> s = 'mississippi'
+# >>> d = defaultdict(int)
+# >>> for k in s:
+# ...     d[k] += 1
+# ...
+# >>> d.items()
+# [('i', 4), ('p', 2), ('s', 4), ('m', 1)]
+
 class Solution:
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
         results = collections.defaultdict(list)
